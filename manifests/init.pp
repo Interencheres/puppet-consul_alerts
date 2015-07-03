5ea387fad8b995b43579f358d7c1daf984dbe172
Olivier <ofontannaud@cpmultimedia.com>
2015-07-03 17:19:14 +0200

    change consul_alerts version

--- manifests/init.pp
+++ manifests/init.pp
@@ -50,7 +50,7 @@
 class consul_alerts (
   $enabled      = true,
   $binary_path  = '/usr/local/bin',
-  $version      = '0.3.1',
+  $version      = '0.4.1',
   $repo_url     = 'https://github.com/Interencheres/consul-alerts/releases/download',
   $arch         = $::architecture,
   $default_url  = true,
